a11c99d9499fbdf249404474dfad5fc36fa519a6
Joao Costa <89090650+jcostacx@users.noreply.github.com>
2023-11-13 09:26:23 +0000

    Update build-ubuntu.sh

--- .build/build-ubuntu.sh
+++ .build/build-ubuntu.sh
@@ -12,24 +12,4 @@ rm -f -r --interactive=never cxinventory.spec
 rm -f -r --interactive=never temp
 tar -czvf ../.dist/cxinventory-ubuntu64.tar.gz -C ../.dist/cxinventory/ubuntu .
 
-# -------------------------------
-# Create cxquerymerger executable
-# -------------------------------
-pyinstaller --clean --noconfirm --onefile --nowindow --distpath=../.dist/cxquerymerger/ubuntu --workpath=temp --paths=../shared ../cxquerymerger/cxquerymerger.py
-cp ../cxquerymerger/src/cxquerymergerconfig.yaml ../.dist/cxquerymerger/ubuntu/config.yaml
-cp ../LICENSE ../.dist/cxquerymerger/ubuntu/LICENSE
-rm -f -r --interactive=never cxquerymerger.spec
-rm -f -r --interactive=never temp
-tar -czvf ../.dist/cxquerymerger-ubuntu64.tar.gz -C ../.dist/cxquerymerger/ubuntu .
-
-# ---------------------------
-# Create cxscanner executable
-# ---------------------------
-pyinstaller --clean --noconfirm --onefile --nowindow --distpath=../.dist/cxscanner/ubuntu --workpath=temp --paths=../shared ../cxscanner/cxscanner.py
-cp ../cxscanner/src/cxscannerconfig.yaml ../.dist/cxscanner/ubuntu/config.yaml
-cp ../LICENSE ../.dist/cxscanner/ubuntu/LICENSE
-rm -f -r --interactive=never cxscanner.spec
-rm -f -r --interactive=never temp
-tar -czvf ../.dist/cxscanner-ubuntu64.tar.gz -C ../.dist/cxscanner/ubuntu .
-
 popd
